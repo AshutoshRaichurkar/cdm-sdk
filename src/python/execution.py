52af7eb2e91effcb51c3addecc200e66840c00f1
AshutoshRaichurkar10 <raichurkar.a@husky.neu.edu>
2025-12-12 00:51:19 -0500

    adding .conf file

--- src/python/execution.py
+++ src/python/execution.py
@@ -1,10 +1,18 @@
 
 from packages.packages import add
 from pyspark.sql import SparkSession
+import os
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+CONFIG_PATH = os.path.join(BASE_DIR, "resources", "config.conf")
 
-print("Hello World")
-print("This is Ashutosh")
+with open(CONFIG_PATH, "r") as f:
+    contents = f.read()
+
+print("=== Contents of config.conf ===")
+print(contents)
+
+print("=== Just testing the code for packages.packages ===")
 final = add(10,15)
 print(final)
    
